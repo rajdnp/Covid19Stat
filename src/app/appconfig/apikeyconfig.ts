fc583d870ba378f2a088d38de4145df4aaddfe75
Rajesh Donepudi <61657297+rajdnp@users.noreply.github.com>
2021-06-11 19:11:04 +0530

    Update apikeyconfig.ts

--- src/app/appconfig/apikeyconfig.ts
+++ src/app/appconfig/apikeyconfig.ts
@@ -9,7 +9,7 @@ export class ApiKeyConfig {
     console.log('Stored key', key);
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
-
+    // Your host and key will be added here directly no manual work needed.
     headers = headers.set('x-rapidapi-key', `${key}`);
     headers = headers.set('x-rapidapi-host', 'covid-19-data.p.rapidapi.com');
     return headers;
